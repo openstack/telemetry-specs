717f7b1a6a5958faad7e6ad904b8f03651dd5c83
Eoghan Glynn <eglynn@redhat.com>
2014-05-20 23:51:09 +0100

    typo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'telemtry-specs'
+project = u'telemetry-specs'
 copyright = u'2013, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
