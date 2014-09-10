a518da9712362554c6a42b5955e7edc076790d5a
Doug Hellmann <doug@doughellmann.com>
2014-09-10 11:48:32 -0400

    Use the current date for the copyright statement
    Change-Id: Ic39e32b56b03f36f0df8a9e65a5ae35c5b690eab

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime
 import os
 import sys
 
@@ -48,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'telemetry-specs'
-copyright = u'2013, OpenStack Foundation'
+copyright = u'%s, OpenStack Foundation' % datetime.date.today().year
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
